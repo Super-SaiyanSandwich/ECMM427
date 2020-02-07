0c90da0b131a84babeac50cdb02fc7476ebeb66e
Super-Saiyan Sandwich <supersaiyansandwiches@gmail.com>
2020-02-07 17:30:26 +0000

    Revert "Merge image_manipulation"
    This reverts commit 757cce1598e7b033d5b029613fcd19e3ac6c4c39, reversing
    changes made to 3b21661f9945ad3704c32b582799e272b1853ab4.

--- RTIe/RTIe_builder/main.cpp
+++ RTIe/RTIe_builder/main.cpp
@@ -1,17 +1,14 @@
 #include <QApplication>
 #include <QPushButton>
 #include <QLabel>
-#include <image_Management.h>
 
 #include "splash_Screen.h"
 
 int main(int argc, char **argv)
 {
     QApplication app (argc, argv);
-    //splashScreen splashScreen;
-    //splashScreen.show();
-    image_Management image_Management;
-    image_Management.show();
+    splashScreen splashScreen;
+    splashScreen.show();
     return app.exec();
 }
 
