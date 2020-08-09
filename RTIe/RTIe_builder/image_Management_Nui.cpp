5bd6d8fba1f078a3b68634df97ebfc9eadd8b246
jmi <jordan_isaac@icloud.com>
2020-08-09 09:37:21 +0100

    Image management windows fix

--- RTIe/RTIe_builder/image_Management_Nui.cpp
+++ RTIe/RTIe_builder/image_Management_Nui.cpp
@@ -169,7 +169,7 @@ void image_Management_Nui::delete_(QStringList file_Names)
     qInfo()<<"result:"<<result;
 
     //if result = 1 | User pressed OK
-    if(result == QDialog::Accepted){
+    if(result == 1){
 
 
 
